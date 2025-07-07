d1ad89480f743f841c0cef5f7c64faca959cfd40
atrh95 <atora.has75@gmail.com>
2025-07-07 14:16:25 +0900

    投稿機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   get "/" => "home#top"
   get "about" => "home#about"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+
+  post "posts/create" => "posts#create"
 end
